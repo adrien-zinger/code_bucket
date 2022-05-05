a7f42c13497794349410de1da224fad4469ba2bd
Adrien Zinger <zinger.ad@gmail.com>
2022-05-05 08:20:48 +0200

    Modifs + add id wf

--- operation_propagation/shell.nix
+++ operation_propagation/shell.nix
@@ -1,12 +1,10 @@
 with (import <nixpkgs> {});
 mkShell {
   buildInputs = [
-    bundler
     (vscode-with-extensions.override {
       vscodeExtensions = with pkgs.vscode-extensions; [
 	      eamodio.gitlens
 	      yzhang.markdown-all-in-one
-        jnoortheen.nix-ide
 	      streetsidesoftware.code-spell-checker
       ];
     })
