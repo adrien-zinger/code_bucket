f1c1134233bfaf42b12adb591ee8103d0374faa2
Adrien Zinger <zinger.ad@gmail.com>
2022-06-07 09:54:29 +0200

    Add example for my result implem + Try trait

---
+++ my_result/src/main.rs
@@ -0,0 +1,61 @@
+#![feature(try_trait_v2)]
+#![feature(never_type)]
+
+#[allow(dead_code)]
+enum MyResult<T, E> {
+    Some(T),
+    None,
+    Err(E),
+}
+
+impl<T, E> std::ops::Try for MyResult<T, E> {
+    type Output = T;
+    type Residual = MyResult<!, E>;
+
+    #[inline]
+    fn from_output(c: T) -> Self {
+        MyResult::Some(c)
+    }
+
+    #[inline]
+    fn branch(self) -> std::ops::ControlFlow<Self::Residual, T> {
+        match self {
+            MyResult::Some(c) => std::ops::ControlFlow::Continue(c),
+            MyResult::Err(e) => std::ops::ControlFlow::Break(MyResult::Err(e)),
+            MyResult::None => std::ops::ControlFlow::Break(MyResult::None),
+        }
+    }
+}
+
+impl<T, E, F: From<E>> std::ops::FromResidual<MyResult<!, E>> for MyResult<T, F> {
+    fn from_residual(x: MyResult<!, E>) -> Self {
+        match x {
+            MyResult::Some(e) => {
+                #[allow(unreachable_code)]
+                let e = From::from(e);
+                MyResult::Some(e)
+            }
+            MyResult::None => MyResult::None,
+            MyResult::Err(e) => MyResult::Err(From::from(e)),
+        }
+    }
+}
+
+fn bar() -> MyResult<String, &'static str> {
+    // MyResult::Some(String::from("haha!"))
+    // MyResult::None
+    MyResult::Err("This is an error")
+}
+
+fn foo() -> MyResult<String, &'static str> {
+    let _ = bar()?;
+    MyResult::Some(String::from("hello world"))
+}
+
+fn main() {
+    match foo() {
+        MyResult::Some(hello) => println!("{}", hello),
+        MyResult::None => println!("nothing O_O"),
+        MyResult::Err(e) => println!("{}", e),
+    }
+}
