4e1fbb54842e09d524ea14d977b977dc618678a2
Adrien Zinger <zinger.ad@gmail.com>
2022-06-14 17:48:26 +0200

    add print/set functions

--- c_classes/main.c
+++ c_classes/main.c
@@ -10,6 +10,8 @@ struct Class {
   void * (* dtor) ( void * self);
   void * (* clone) (const void * self);
   int (* differ) (const void * self, const void * other);
+  void (* set) (void * self, va_list * app);
+  void (* print) (const void * self);
 };
 
 struct String {
@@ -34,6 +36,17 @@ void * new(const void * _class, ...) {
   return p;
 }
 
+void print(const void * _class) {
+  (* (const struct Class **) _class)->print(_class);
+}
+
+void set(void * _class, ...) {
+  va_list args;
+  va_start(args, _class);
+  (* (const struct Class **) _class)->set(_class, &args);
+  va_end(args);
+}
+
 /**** IMPLEMENTATION DE LA CLASSE STRING ***/
 const void * String;
 
@@ -56,6 +69,21 @@ static void * String_clone(const void * _self) {
   return new(String, self->text);
 }
 
+static void String_set(void * _self, va_list * args) {
+  struct String * self = _self;
+  const char * str = va_arg(* args, const char *);
+  if (strcmp(self->text, str) == 0)
+    return;
+  if (strlen(self->text) != strlen(str))
+    self->text = realloc(self->text, strlen(str) + 1);
+  strcpy(self->text, str);
+}
+
+static void String_print(const void * _self) {
+  const struct String * self = _self;
+  printf("%s\n\0", self->text);
+}
+
 static int String_differ(const void * _self, const void * _other) {
   const struct String * self = _self;
   const struct String * other = _other;
@@ -69,13 +97,19 @@ const struct Class _String = {
   String_ctor,
   String_dtor,
   String_clone,
-  String_differ
+  String_differ,
+  String_set,
+  String_print,
 };
 
 const void * String = & _String;
 
 int main() {
-  struct String * str = new(String, "hello");
-  printf("%s\n\0", str->text);
+  void * str = new(String, "hello");
+  print(str);
+  set(str, "yeeeeeeh yeeeeeh yeh");
+  print(str);
+  set(str, "bip");
+  print(str);
   return 0;
 }
