cda5599d3b962a6d8cfbce3b0a990b42d3b8d369
Adrien Zinger <zinger.ad@gmail.com>
2023-02-23 14:47:23 +0100

    fix nom if pattern parser

--- parser_tests/if_pattern/nom_if_pattern/src/parser.rs
+++ parser_tests/if_pattern/nom_if_pattern/src/parser.rs
@@ -12,7 +12,7 @@ use nom::{
     branch::alt,
     bytes::complete::tag,
     character::complete::{alphanumeric1, multispace0},
-    combinator::recognize,
+    combinator::{eof, recognize},
     error::VerboseError,
     multi::many0_count,
     sequence::{delimited, pair, Tuple},
@@ -22,13 +22,47 @@ use nom::{
 pub type Res<T, U> = IResult<T, U, VerboseError<T>>;
 
 pub fn stmt(input: &str) -> Res<&str, &str> {
-    alt((matched, unmatched))(input)
+    // Peu importe l'ordre ici
+    alt((bounded_stmt_unmatched, bounded_stmt_matched))(input)
+}
+
+pub fn bounded_stmt_unmatched(input: &str) -> Res<&str, &str> {
+    match (unmatched, eof).parse(input) {
+        Ok((input, (_, rest))) => {
+            println!("bounded stmt unmatched done");
+            Ok((input, rest))
+        }
+        Err(err) => {
+            println!("error bounded stmt unmatched");
+            Err(err)
+        }
+    }
+}
+
+pub fn bounded_stmt_matched(input: &str) -> Res<&str, &str> {
+    match (matched, eof).parse(input) {
+        Ok((input, (_, rest))) => {
+            println!("bounded stmt matched done");
+            Ok((input, rest))
+        }
+        Err(err) => {
+            println!("error bounded stmt matched");
+            Err(err)
+        }
+    }
 }
 
 fn ext_matched(input: &str) -> Res<&str, &str> {
-    let (input, (_, expr, _, _, _, rest)) =
-        (token_if, expr, token_then, matched, token_else, matched).parse(input)?;
-    println!("ext matched: {expr}");
+    let (input, _) = token_if(input)?;
+    let (input, (_expr, _, _, _, rest)) =
+        match (expr, token_then, matched, token_else, matched).parse(input) {
+            Ok(res) => res,
+            Err(err) => {
+                println!("error ext matched");
+                return Err(err);
+            }
+        };
+    println!("ext matched done");
     Ok((input, rest))
 }
 
@@ -41,15 +75,29 @@ fn unmatched(input: &str) -> Res<&str, &str> {
 }
 
 fn ext_unmatched_simple(input: &str) -> Res<&str, &str> {
-    let (input, (_, expr, _, rest)) = (token_if, expr, token_then, matched).parse(input)?;
-    println!("ext unmatched (simple): {expr}");
-    Ok((input, rest))
+    match (token_if, expr, token_then, matched).parse(input) {
+        Ok((input, (_, _expr, _, rest))) => {
+            println!("ext unmatched (simple) done");
+            Ok((input, rest))
+        }
+        Err(err) => {
+            println!("error ext unmatched (simple)");
+            Err(err)
+        }
+    }
 }
 
 fn ext_unmatched_recursion(input: &str) -> Res<&str, &str> {
-    let (input, (_, _, _, _, _, rest)) =
-        (token_if, expr, token_then, matched, token_else, unmatched).parse(input)?;
-    Ok((input, rest))
+    match (token_if, expr, token_then, matched, token_else, unmatched).parse(input) {
+        Ok((input, (_, _, _, _, _, rest))) => {
+            println!("ext unmatched (recursion) done");
+            Ok((input, rest))
+        }
+        Err(err) => {
+            println!("error ext_unmatched (recursion)");
+            Err(err)
+        }
+    }
 }
 
 fn token_then(input: &str) -> Res<&str, &str> {
@@ -76,14 +124,12 @@ fn expr(input: &str) -> Res<&str, &str> {
 }
 
 fn stmt_final(input: &str) -> Res<&str, &str> {
-    let (input, stmt) = delimited(
+    delimited(
         multispace0,
         recognize(pair(
             tag("stmt"),
             many0_count(alt((alphanumeric1, tag("_")))),
         )),
         multispace0,
-    )(input)?;
-    println!("stmt: {stmt}");
-    Ok((input, stmt))
+    )(input)
 }
