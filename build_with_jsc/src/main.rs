9cbd2b2d5a1c693727596d337ab94ef3d1fc623e
Adrien Zinger <zinger.ad@gmail.com>
2023-03-31 15:05:25 +0200

    add class type

--- build_with_jsc/src/main.rs
+++ build_with_jsc/src/main.rs
@@ -1,14 +1,12 @@
-use std::ffi::{c_char, CString};
-
 use rusty_jsc::{JSContext, JSObject, JSValue};
-use rusty_jsc_macros::callback;
+use rusty_jsc_macros::constructor;
 use rusty_jsc_sys::*;
 
 // The JavaScript code calls this Rust function.
-//#[callback]
-//fn console_log(_context: JSContext) {
-//    println!("hello from Rust land!");
-//}
+#[constructor]
+fn constructor(_context: JSContext, _constructor: JSObject) {
+    println!("hello from Rust land!");
+}
 
 unsafe extern "C" fn console_log(
     ctx: JSContextRef,
@@ -40,6 +38,7 @@ unsafe extern "C" fn console_log(
     println!("arg count {}", argument_count);
     println!("{}", JSValueGetType(ctx, arguments.read()));
     println!("{}", JSValueIsDate(ctx, arguments.read()));
+    println!("{}", JSValueIsUndefined(ctx, arguments.read()));
     println!(
         "{:?}",
         JSValueToStringCopy(ctx, arguments.read(), std::ptr::null_mut())
@@ -48,64 +47,25 @@ unsafe extern "C" fn console_log(
     JSValueMakeUndefined(ctx)
 }
 
-fn create_console_class() -> JSClassRef {
-    let ln = Box::into_raw(Box::new(CString::new("log").unwrap()));
-    let log_fn = Box::into_raw(Box::new(JSStaticFunction {
-        name: ln as *const c_char,
-        callAsFunction: Some(console_log),
-        attributes: kJSPropertyAttributeDontDelete as u32 | kJSPropertyAttributeReadOnly,
-    })) as *const JSStaticFunction;
-
-    let log = [log_fn, Box::into_raw(Box::new(JSStaticFunction::null()))].as_ptr()
-        as *const JSStaticFunction;
-
-    //let values = [Box::into_raw(Box::new(JSStaticValue { name: "hello", getProperty: Some(console_log), setProperty: Some(console_log), attributes: todo!() }))]
-
-    println!("log created");
-
-    unsafe {
-        let mut class_definition = kJSClassDefinitionEmpty;
-        class_definition.className = "console".as_bytes().as_ptr() as _;
-        class_definition.attributes = kJSClassAttributeNone;
-        class_definition.staticValues = std::ptr::null();
-        class_definition.staticFunctions = log;
-        JSClassCreate(Box::into_raw(Box::new(class_definition)))
-    }
-}
-
 fn main() {
     let mut context = JSContext::default();
     let _callback = JSValue::callback(&context, Some(console_log));
     let mut global = context.get_global_object();
 
-    let obj_console: JSObjectRef = unsafe {
-        JSObjectMake(
-            context.get_ref(),
-            create_console_class(),
-            std::ptr::null_mut(),
-        )
-    };
+    let file_class = JSObject::class(&context, "File", Some(constructor));
 
-    println!("obj console done");
-    global.set_property(
-        &context,
-        "console".to_string(),
-        JSValue::from(obj_console as JSValueRef),
-    );
-    println!("done");
+    global.set_property(&context, "File".to_string(), file_class.into());
 
-    let mut _console_def: JSObject = global
-        .get_property(&context, "console".to_string())
-        .expect("Cannot find console")
-        .into();
-    let log_fn = _console_def
-        .get_property(&context, "log".to_string())
-        .expect("cannot find log??");
+    //unsafe { JSObjectMakeConstructor(context.get_ref(), my_class, Some(constructor_fn)) };
+    //let log_fn = _console_def
+    //    .get_property(&context, "test".to_string())
+    //    .expect("cannot find log??");
     //assert!(!log_fn.is_undefined(&context), "log undefined");
     //_console_def.set_property(&context, "log".to_string(), _callback);
 
+    println!("start evaluation");
     //context.evaluate_script("console", 1).expect("succeed 1");
     context
-        .evaluate_script("new console().log(\"hello you\")", 1)
+        .evaluate_script("var f = new File();", 1)
         .expect("succeed 2");
 }
