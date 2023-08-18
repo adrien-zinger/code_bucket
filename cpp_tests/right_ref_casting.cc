63664b15985531855461f0a807f7578e855fc698
Adrien Zinger <zinger.ad@gmail.com>
2023-08-18 14:22:28 +0200

    Create right_ref_casting.cc

---
+++ cpp_tests/right_ref_casting.cc
@@ -0,0 +1,17 @@
+/* c++11 */
+
+#include <iostream>
+
+void f(float &&) { std::cout << "f"; }
+void f(int &&) { std::cout << "i"; }
+
+template <typename... T>
+void g(T &&... v)
+{
+    (f(v), ...);
+}
+
+int main()
+{
+    g(1.0f, 2);
+}
